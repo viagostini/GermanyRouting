d3530c9e988883b81bd6931fd185fd3a659ce4b4
Vinicius Agostini <vinicius.agostini19@gmail.com>
2024-03-12 14:56:41 +0100

    Implement basic DFS and BFS

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 package com.github.viagostini
 
+import kotlin.collections.ArrayDeque
+
 fun main() {
     val graph = Network()
 
@@ -13,15 +15,25 @@ fun main() {
     graph.addRide(Ride(City("Munich"), City("Frankfurt")));
     graph.addRide(Ride(City("Cologne"), City("Frankfurt")));
 
-    println(graph.ridesFrom(City("Berlin")))
-    println(graph.ridesFrom(City("Munich")))
-    println(graph.ridesFrom(City("Cologne")))
-    println(graph.ridesFrom(City("Frankfurt")))
-    println(graph)
+    val path = graph.anyPathDFS(City("Berlin"), City("Frankfurt"))
+    path.print()
 }
 
 data class City(val name: String)
 data class Ride(val from: City, val to: City)
+typealias Path = List<Ride>
+
+fun Path?.print() {
+    if (this == null) {
+        println("No path found!")
+        return
+    }
+
+    val source = this.first().from.name
+    val rest = this.joinToString(separator = " -> ") { it.to.name }
+    println("$source -> $rest")
+}
+
 
 class Network {
     private val adjacencyMap = mutableMapOf<City, MutableList<Ride>>()
@@ -40,8 +52,54 @@ class Network {
         adjacencyMap[from]!!.add(ride)
     }
 
-    fun ridesFrom(city: City): List<Ride> {
+    private fun ridesFrom(city: City): List<Ride> {
         return adjacencyMap[city] ?: throw IllegalArgumentException("The city '$city' is not in the graph")
+    }
+
+    fun anyPathDFS(from: City, to: City): Path? {
+        data class State(val city: City, val path: Path)
+
+        val visited = mutableSetOf<City>()
+        val stack = ArrayDeque<State>()
+
+        stack.addLast(State(from, emptyList()))
+
+        while (stack.isNotEmpty()) {
+            val (city, path) = stack.removeLast()
+
+            if (city == to) return path
+
+            if (city in visited) continue
+
+            visited.add(city)
+
+            ridesFrom(city).forEach { stack.addLast(State(it.to, path + it)) }
+        }
+
+        return null
+    }
+
+    fun anyPathBFS(from: City, to: City): Path? {
+        data class State(val city: City, val path: Path)
+
+        val visited = mutableSetOf<City>()
+        val queue = ArrayDeque<State>()
+
+        queue.addLast(State(from, emptyList()))
+
+        while (queue.isNotEmpty()) {
+            val (city, path) = queue.removeFirst()
+
+            if (city == to) return path
+
+            if (city in visited) continue
+
+            visited.add(city)
+
+            ridesFrom(city).forEach { queue.addLast(State(it.to, path + it)) }
+        }
+
+        return null
     }
 
     override fun toString(): String {
